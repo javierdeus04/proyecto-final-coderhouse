6be1bd6b5558486bcfed722cefc2b96ee01878aa
Javier <javier.deus04@gmail.com>
2022-10-25 17:59:21 -0300

    back up

--- coder/models.py
+++ coder/models.py
@@ -10,3 +10,14 @@ class Paciente(models.Model):
     def __str__(self):
         return f'ID: {self.id}, Nombre:{self.nombre}, Apellido:{self.apellido}, CI:{self.numero_ci}'
 
+class Funcionario(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    fecha_de_nacimiento = models.DateField()
+    numero_ci = models.IntegerField(max_length=8)
+    numero_cel = models.IntegerField(max_length=9)
+    numero_func = models.IntegerField(max_length=5)
+
+    def __str__(self):
+        return f'ID: {self.id}, Nombre:{self.nombre}, Apellido:{self.apellido}, Número de funcionario:{self.numero_func}'
+
